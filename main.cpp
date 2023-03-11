8134a96ee092bbb64c294f9ee7df82f46af88b77
21520147-PhamTuanAnh <21520147@gm.uit.edu.vn>
2023-03-11 23:15:04 +0700

    update

--- main.cpp
+++ main.cpp
@@ -1,41 +1,34 @@
-#include <d3d9.h> // direct3d - directx 9
+#include <d3d9.h>
+#include <time.h>
 #include "WindowClass.h"
-
-
-// include the Direct3D Library file
 #pragma comment (lib, "d3d9.lib")
 
+#define WINDOWCLASS_WINDOWPROC WindowProc
+#define WINDOWCLASS_SIZE sizeof(WNDCLASSEX)
+#define WINDOWCLASS_CLASSNAME L"WindowClass"
+#define WINDOWCLASS_STYLE CS_HREDRAW | CS_VREDRAW 
+#define WINDOWCLASS_BACKGROUND (HBRUSH)COLOR_WINDOW
+#define WINDOWCLASS_CURSOR LoadCursor(NULL, IDC_ARROW)
 
-#define WC_STYLE CS_HREDRAW | CS_VREDRAW
-#define WC_CBSIZE sizeof(WNDCLASSEX)
-
-#define WC_HCURSOR LoadCursor(NULL, IDC_ARROW)
-#define WC_HBRBACKGROUND (HBRUSH)COLOR_WINDOW
-
-#define WC_LPFNWNDPROC WindowProc
-#define WC_LPSZCLASSNAME L"WindowClass1"
-
-#define WINDOW_EX_STYLE NULL
-#define WINDOW_X_POS 0
-#define WINDOW_Y_POS 0
-#define WINDOW_TITLE L"Our First Windowed Program"
-#define WINDOW_WIDTH 500
+#define WINDOW_WIDTH 1000
 #define WINDOW_HEIGHT 500
 #define WINDOW_PARENT NULL
 #define WINDOW_MENUBAR NULL
+#define WINDOW_X_POSITION 0
+#define WINDOW_Y_POSITION 0
+#define WINDOW_EXTRA_STYLE NULL
+#define WINDOW_TITLE L"MainWindow"
+#define WINDOW_STYLE WS_EX_TOPMOST | WS_POPUP | WS_VISIBLE
 
+LPDIRECT3D9 d3d = NULL;
+LPDIRECT3DDEVICE9 d3ddev = NULL;
+LPDIRECT3DSURFACE9 surface = NULL;
+LPDIRECT3DSURFACE9 backBuffer = NULL;
 
-// global declarations
-LPDIRECT3D9 d3d;    // the pointer to our Direct3D interface
-LPDIRECT3DDEVICE9 d3ddev;    // the pointer to the device class
-
-// function prototypes
-void initD3D(HWND hWnd);    // sets up and initializes Direct3D
-void render_frame(void);    // renders a single frame
-void cleanD3D(void);    // closes Direct3D and releases memory
-
-
-LRESULT CALLBACK WindowProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam);
+void GameInit(HWND);
+void GameRun(HWND);
+void GameEnd(HWND);
+LRESULT CALLBACK WindowProc(HWND, UINT, WPARAM, LPARAM);
 
 
 int WINAPI WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPSTR lpCmdLine, _In_ int nCmdShow)
@@ -43,19 +36,25 @@ int WINAPI WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _
 
 	WindowClass* wc = new WindowClass();
 	wc->Clean();
-	wc->SetStyle(WC_STYLE)->SetCbSize(WC_CBSIZE)->SetLpszClassName(WC_LPSZCLASSNAME)
-		->SetHCursor(WC_HCURSOR)->SetHInstance(hInstance)->SetHbrBackground(WC_HBRBACKGROUND)->SetLpfnWndProc(WC_LPFNWNDPROC);
+	wc->SetHInstance(hInstance)
+		->SetStyle(WINDOWCLASS_STYLE)
+		->SetCbSize(WINDOWCLASS_SIZE)
+		->SetHCursor(WINDOWCLASS_CURSOR)
+		->SetLpfnWndProc(WINDOWCLASS_WINDOWPROC)
+		->SetLpszClassName(WINDOWCLASS_CLASSNAME)
+		->SetHbrBackground(WINDOWCLASS_BACKGROUND)
+		;
 	wc->Build();
 
 
 	HWND hWnd = CreateWindowEx
 	(
-		WINDOW_EX_STYLE,
-		WC_LPSZCLASSNAME,
+		WINDOW_EXTRA_STYLE,
+		WINDOWCLASS_CLASSNAME,
 		WINDOW_TITLE,
-		WS_OVERLAPPEDWINDOW,
-		WINDOW_X_POS,
-		WINDOW_Y_POS,
+		WINDOW_STYLE,
+		WINDOW_X_POSITION,
+		WINDOW_Y_POSITION,
 		WINDOW_WIDTH,
 		WINDOW_HEIGHT,
 		WINDOW_PARENT,
@@ -66,123 +65,116 @@ int WINAPI WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _
 
 	ShowWindow(hWnd, nCmdShow);
 
-	// set up and initialize Direct3D
-	initD3D(hWnd);
-
+	GameInit(hWnd);
 
 	MSG msg;
+	BOOL isDone = FALSE;
 
-	while (1)
+	while (!isDone)
 	{
-		// Check to see if any messages are waiting in the queue
-		while (PeekMessage(&msg, NULL, 0, 0, PM_REMOVE))
+		if (PeekMessage(&msg, NULL, 0, 0, PM_REMOVE))
 		{
-			// Translate the message and dispatch it to WindowProc()
+			if (msg.message == WM_QUIT)
+			{
+				OutputDebugString(L"\n\nWM_QUIT called\n\n");
+				isDone = TRUE;
+			}
+
 			TranslateMessage(&msg);
 			DispatchMessage(&msg);
 		}
-
-		// If the message is WM_QUIT, exit the while loop
-		if (msg.message == WM_QUIT)
+		else
 		{
-			OutputDebugString(L"\n\nWM_QUIT called\n\n");
-			break;
+			GameRun(hWnd);
 		}
-
-		render_frame();
-		// Run game code here
-		// ...
-		// ...
 	}
 
-	// clean up DirectX and COM
-	cleanD3D();
+	GameEnd(hWnd);
 
-	// return this part of the WM_QUIT message to Windows
 	return msg.wParam;
-
-	return 0;
 }
 
-// this is the main message handler for the program
 LRESULT CALLBACK WindowProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 {
-	// sort through and find what code to run for the message given
 	switch (message)
 	{
-		// this message is read when the window is closed
+
 	case WM_DESTROY:
-	{
 		OutputDebugString(L"\n\nWM_DESTROY called\n\n");
-		// close the application entirely
 		PostQuitMessage(0);
 		return 0;
-	} break;
+		break;
+
 	case WM_CLOSE:
-	{
 		OutputDebugString(L"\n\nWM_CLOSE called\n\n");
-		// close the application entirely
 		PostQuitMessage(0);
 		return 0;
-	} break;
+		break;
+
 	}
 
-	// Handle any messages the switch statement didn't
 	return DefWindowProc(hWnd, message, wParam, lParam);
 }
 
-// this function initializes and prepares Direct3D for use
-void initD3D(HWND hWnd)
+void GameInit(HWND hWnd)
 {
-	d3d = Direct3DCreate9(D3D_SDK_VERSION);    // create the Direct3D interface
+	d3d = Direct3DCreate9(D3D_SDK_VERSION);
 
-	D3DPRESENT_PARAMETERS d3dpp;    // create a struct to hold various device information
+	D3DPRESENT_PARAMETERS d3dpp;
+	ZeroMemory(&d3dpp, sizeof(d3dpp));
 
-	ZeroMemory(&d3dpp, sizeof(d3dpp));    // clear out the struct for use
-	d3dpp.Windowed = TRUE;    // program windowed, not fullscreen
-	d3dpp.SwapEffect = D3DSWAPEFFECT_DISCARD;    // discard old frames
-	d3dpp.hDeviceWindow = hWnd;    // set the window to be used by Direct3D
-	
-	// create a device class using this information and information from the d3dpp stuct
-	d3d->CreateDevice(D3DADAPTER_DEFAULT,
+	d3dpp.Windowed = TRUE;
+	d3dpp.hDeviceWindow = hWnd;
+	d3dpp.BackBufferWidth = WINDOW_WIDTH;
+	d3dpp.BackBufferHeight = WINDOW_HEIGHT;
+	d3dpp.SwapEffect = D3DSWAPEFFECT_DISCARD;
+	d3dpp.BackBufferFormat = D3DFMT_X8R8G8B8;
+
+	d3d->CreateDevice
+	(
+		D3DADAPTER_DEFAULT,
 		D3DDEVTYPE_HAL,
 		hWnd,
 		D3DCREATE_SOFTWARE_VERTEXPROCESSING,
 		&d3dpp,
-		&d3ddev);
-}
-int GSpd = 1;
-int BSpd = 1;
-int RSpd = 1;
-int R = 255;
-int G = 255;
-int B = 255;
-// this is the function used to render a single frame
-void render_frame(void)
-{
-	// clear the window to a deep blue
-	d3ddev->Clear(0, NULL, D3DCLEAR_TARGET, D3DCOLOR_XRGB(R, G, B), 1.0f, 0);
+		&d3ddev
+	);
 
-	d3ddev->BeginScene();    // begins the 3D scene
+	d3ddev->Clear(0, NULL, D3DCLEAR_TARGET, D3DCOLOR_XRGB(255, 255, 255), 1.0f, 0);
 
-	// do 3D rendering on the back buffer here
+	d3ddev->GetBackBuffer(0, 0, D3DBACKBUFFER_TYPE_MONO, &backBuffer);
+	d3ddev->CreateOffscreenPlainSurface(100, 100, D3DFMT_X8R8G8B8, D3DPOOL_DEFAULT, &surface, NULL);
 
-	if (R == 255 || R == 0) RSpd = -RSpd;
-
-	if (G == 255 || G == 0) GSpd = -GSpd;
-	G += GSpd;
-
-	if (B == 255 || B == 0) BSpd = -BSpd;
-	B += BSpd;
-
-	d3ddev->EndScene();    // ends the 3D scene
-
-	d3ddev->Present(NULL, NULL, NULL, NULL);    // displays the created frame
+	srand(time(NULL));
 }
 
-// this is the function that cleans up Direct3D and COM
-void cleanD3D(void)
+void GameRun(HWND hWnd)
 {
-	d3ddev->Release();    // close and release the 3D device
-	d3d->Release();    // close and release Direct3D
+	Sleep(500);
+
+	RECT rect{};
+
+	d3ddev->Clear(0, NULL, D3DCLEAR_TARGET, D3DCOLOR_XRGB(255, 255, 255), 1.0f, 0);
+
+	d3ddev->BeginScene();
+
+	rect.left = rand() % WINDOW_WIDTH / 2;
+	rect.top = rand() % WINDOW_WIDTH / 2;
+	rect.right = rect.left + rand() % WINDOW_WIDTH / 2;
+	rect.bottom = rect.bottom + rand() % WINDOW_WIDTH / 2;
+
+	d3ddev->ColorFill(surface, NULL, D3DCOLOR_XRGB(0, 0, 0));
+	d3ddev->StretchRect(surface, NULL, backBuffer, &rect, D3DTEXF_NONE);
+
+	d3ddev->EndScene();
+
+	d3ddev->Present(NULL, NULL, NULL, NULL);
+}
+
+void GameEnd(HWND hWnd)
+{
+	surface->Release();
+	backBuffer->Release();
+	d3ddev->Release();
+	d3d->Release();
 }
