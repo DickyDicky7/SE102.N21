945bb4f1c692c9eccd1a6637431e5fae63205dc2
DickyDicky7 <87304213+DickyDicky7@users.noreply.github.com>
2023-04-04 08:39:31 +0700

    Update WallTurretLeft60State.cpp

--- WallTurretLeft60State.cpp
+++ WallTurretLeft60State.cpp
@@ -7,7 +7,7 @@ WallTurretLeft60State::~WallTurretLeft60State() {}
 void WallTurretLeft60State::Exit(WallTurret& wallTurret) {}
 
 void WallTurretLeft60State::Render(WallTurret& wallTurret) {
-	wallTurret.SetAnimation(WALL_TURRET_ANIMATIONS_ID::LEFT_60, wallTurret.GetPosition(), wallTurret.GetMovingDirection());
+	wallTurret.SetAnimation(WALL_TURRET_ANIMATION_ID::LEFT_60, wallTurret.GetPosition(), wallTurret.GetMovingDirection());
 }
 
 void WallTurretLeft60State::Enter(WallTurret& wallTurret) {}
