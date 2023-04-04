f046ec9e4278bfadcb35b2e8e3dc4b75d2ee27f1
DickyDicky7 <87304213+DickyDicky7@users.noreply.github.com>
2023-04-04 08:19:42 +0700

    Update WallTurretDownState.cpp

--- WallTurretDownState.cpp
+++ WallTurretDownState.cpp
@@ -7,7 +7,7 @@ WallTurretDownState::~WallTurretDownState() {}
 void WallTurretDownState::Exit(WallTurret& wallTurret) {}
 
 void WallTurretDownState::Render(WallTurret& wallTurret) {
-	wallTurret.SetAnimation(WALL_TURRET_ANIMATIONS_ID::DOWN, wallTurret.GetPosition(), wallTurret.GetMovingDirection());
+	wallTurret.SetAnimation(WALL_TURRET_ANIMATION_ID::DOWN, wallTurret.GetPosition(), wallTurret.GetMovingDirection());
 }
 
 void WallTurretDownState::Enter(WallTurret& wallTurret) {}
