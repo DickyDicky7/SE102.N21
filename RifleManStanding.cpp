6e232f4ab830fb4860bc8dbe0ca5ba8dfca1ca80
DickyDicky7 <87304213+DickyDicky7@users.noreply.github.com>
2023-04-30 13:42:56 +0700

    Update RifleManStanding.cpp

--- RifleManStanding.cpp
+++ RifleManStanding.cpp
@@ -6,8 +6,8 @@ RifleManStanding::RifleManStanding() : Entity(), HasAnimations()
 	Enemy::self = this;
 	Entity::self = this;
 
-	this->vx = 0.0f;
-	this->vy = 0.0f;
+	this->vx = 1.0f;
+	this->vy = 1.0f;
 	this->ax = 0.1f;
 	this->ay = 0.1f;
 	this->position.x = 300;
