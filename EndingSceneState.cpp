31a37292441375a4588368a3c8437059cc1a101d
AkiraNoob <narutoducphuong76@gmail.com>
2023-06-23 00:26:42 +0700

    end scene

--- EndingSceneState.cpp
+++ EndingSceneState.cpp
@@ -26,6 +26,14 @@ void EndingSceneState::Render(Scene& scene)
 
 SceneState* EndingSceneState::Update(Scene& scene)
 {
+	std::vector<std::tuple<SPRITE_ID, TIME>>& frames = std::get<
+		std::vector<std::tuple<SPRITE_ID, TIME>>>(GraphicsDatabase::animations[SCENE_ANIMATION_ID::ENDING_FRAME]);
+
+	if (std::cmp_greater_equal(scene.GetCurrentFrame() + 1, frames.size()))
+	{
+		return new CreditSceneState();
+	}
+
 	return NULL;
 }
 
